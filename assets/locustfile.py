e68ea1378e0b549f83ec00d2d04135d79e44074d
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-14 11:56:58 +0200

    Fixed indent

--- assets/locustfile.py
+++ assets/locustfile.py
@@ -4,8 +4,8 @@ class MyUser(HttpUser):
     wait_time = between(5, 15)
 
     @task(4)
-        def index(self):
-            self.client.get("/")
+    def index(self):
+        self.client.get("/")
 
     @task(1)
     def productpage(self):
