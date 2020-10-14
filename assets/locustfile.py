7cd86a8fc986cd585b651c7b5a27fe3a8d9b9293
Florian JUDITH <florian.judith.b@gmail.com>
2020-10-14 10:52:17 +0200

    Fixed class name

--- assets/locustfile.py
+++ assets/locustfile.py
@@ -1,6 +1,6 @@
 import base64
 
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task
 from random import randint, choice
 from io import BytesIO
 
@@ -13,7 +13,7 @@ class WebTasks(TaskSet):
         self.client.get("/productpage")
 
 
-class Web(HttpLocust):
+class Web(HttpUser):
     task_set = WebTasks
     min_wait = 0
     max_wait = 0
